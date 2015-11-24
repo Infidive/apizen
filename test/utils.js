5c8c2e7df425807e069afe20d0d4e08ff6181af7
Rutaihwa <rutaihwa@gmail.com>
2015-11-24 22:41:09 +0300

    Ability to generate keys

--- test/utils.js
+++ test/utils.js
@@ -4,6 +4,9 @@
 var Code = require('code');
 var Lab = require('lab');
 var Path = require('path');
+var Utils = require('../lib/utils');
+var Shortid = require('shortid');
+var Wagner = require('wagner-core');
 
 // Test shortcuts
 var lab = exports.lab = Lab.script();
@@ -15,7 +18,7 @@ describe('utils - utils/index', function () {
 
     it('Checks if lsdir returns array of only wanted files', function (done) {
 
-        var files = require('../lib/utils').lsdir(Path.join(__dirname),'utils.js');
+        var files = Utils.lsdir(Path.join(__dirname),'utils.js');
 
         expect(files).to.be.array();
         expect(files).not.to.contain('utils.js');
@@ -24,4 +27,34 @@ describe('utils - utils/index', function () {
         done();
 
     });
+
+    it('Checks keyGen', function (done) {
+
+        var uuidv4format = /[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i;
+
+        var shortid = null;
+        var uuid = null;
+
+        Wagner.task('generateIds', function (callback){
+
+            shortid = Utils.keyGen.shortid();
+            uuid = Utils.keyGen.uuid();
+            var error = null;
+
+            if (!shortid || !uuid) {
+                error = new Error('Couln\'t generate a key');
+            }
+
+            callback(error, 'generated');
+        });
+
+        Wagner.invokeAsync(function (error, generateIds) {
+
+            expect(error).not.to.exist();
+            expect(Shortid.isValid(shortid), 'An instance of shortid').to.be.true();
+            expect(uuid).match(uuidv4format);
+            done();
+        });
+    });
+
 });
