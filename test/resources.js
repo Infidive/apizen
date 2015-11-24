c42144ad623e7df48c649c37ce847fa6295ae3dd
Rutaihwa <rutaihwa@gmail.com>
2015-11-25 01:15:31 +0300

    Playing with idea of generating barebones routes, that one could extend without pain

--- test/resources.js
+++ test/resources.js
@@ -4,6 +4,8 @@
 var Hapi = require('hapi');
 var Code = require('code');
 var Lab = require('lab');
+var Resource = require('../lib/resources').resource;
+var _ = require('lodash');
 
 // Test shortcuts
 var lab = exports.lab = Lab.script();
@@ -16,10 +18,8 @@ describe('resources /index', function () {
 
     it('Throw error when attempt to create an invalid resource', function (done) {
 
-        var resource = require('../lib/resources/').resource;
-
         try {
-            var testresource = new resource();
+            var testresource = new Resource();
         } catch (error){
 
             expect(error).to.exist();
@@ -27,6 +27,35 @@ describe('resources /index', function () {
         }
 
         expect(testresource).not.to.exist();
+        done();
+    });
+
+    it('Checks that resource does not generate routes when no genroutes have been passed', function (done) {
+
+        var testresource = new Resource({
+            name: 'foo',
+            endpoint: '/foo'
+        });
+
+        expect(testresource).to.exist();
+        expect(testresource.routes).to.be.array();
+        expect(testresource.routes.length).to.equal(0);
+        done();
+    });
+
+    it('Checks that resource generates routes when genroutes have been passed', function (done) {
+
+        var testresource = new Resource({
+            name: 'bar',
+            endpoint: 'bar',
+            genroutes: true
+        });
+
+        expect(testresource).to.exist();
+        expect(testresource.routes).to.be.array();
+        expect(testresource.routes.length).to.above(0);
+        var index = testresource.getRouteIndex('index_get_bar');
+        expect(index).to.equal(0); // that is the first route generated
         done();
     });
 });
